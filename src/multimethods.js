6cf99387d816004777bc6a2cb6f5cc9c1544124e
Gerard Paapu <sharkbrainguy@gmail.com>
2012-04-11 12:44:41 +1200

    refatored Generic to behave more like a javascript constructor

--- src/multimethods.js
+++ src/multimethods.js
@@ -22,80 +22,103 @@
 
 
     Generic = function (_default, _table, _overrides) {
-        var dispatcher, table, overrides;
-
-        dispatcher = function () {
-            var args = slice.call(arguments),
-                i    = table.length,
-                method;
-
-            while (i--) {
-                method = table[i];
-
-                if (method.contract.argumentsMatch(args)) {
-                    return method.apply(this, args); 
-                } 
-            }
-
-            return dispatcher.__default__.apply(this, args);
+        var dispatcher = function () {
+            return Generic.prototype.dispatch.call(dispatcher, arguments, this);
         };
 
-        table     = dispatcher.__table__     = (_table     ? slice.call(_table)     : []);
-        overrides = dispatcher.__overrides__ = (_overrides ? slice.call(_overrides) : []);
-        _default  = dispatcher.__default__   = (_default   ? _default : function () { throw new NoMatchingMethodError(slice.call(arguments)); });
+        dispatcher.__table__     = (_table     ? slice.call(_table)     : []);
+        dispatcher.__overrides__ = (_overrides ? slice.call(_overrides) : []);
+        dispatcher.__default__   = _default  || function () {
+            throw new NoMatchingMethodError(slice.call(arguments));
+        };
 
-        dispatcher.defineMethod = function (argTypes, fn) {
-            var contract, method;
-
-            contract = new Signature();
-            contract.argTypes = argTypes;
-            contract.returnType = Type.Any;
-
-            method = contract.wrap(fn);
-
-            table.push(method);
-
-            // Maintain order of signature specificity
-            table.sort(function (a, b) {
-                var index_a, index_b, max, i;
-
-                for (i = 0, max = overrides.length; i < max; i++) {
-                    index_a = overrides[i].indexOf(a);
-                    index_b = overrides[i].indexOf(b);
-
-                    if (index_a != -1 && index_b != -1) {
-                        return index_a < index_b ? MORE : LESS;
-                    }
-                }
-
-                return WrappedFunction.moreSpecificThan(a, b) ? MORE : LESS;
-            });
+        // The methods on dispatcher should all delegate to the 
+        // Generic prototype so that Generic behaves as close to
+        // a normal javascript constructor as possible
+        // `dispatcher instanceof Generic` will still fail
+        dispatcher.defineMethod = function () {
+            return Generic.prototype.defineMethod.apply(this, arguments);
         };
 
         dispatcher.defineMethods = function () {
-            var max = arguments.length - 1,
-                i;
-
-            for (i = 0; i < max; i += 2) {
-                this.defineMethod.call(this, arguments[i], arguments[i + 1]);
-            }
+            return Generic.prototype.defineMethods.apply(this, arguments);
         };
 
-        dispatcher.prefer = function (a, b) {
-            overrides.unshift([a, b]); 
+        dispatcher.prefer = function () {
+            return Generic.prototype.prefer.apply(this, arguments);
         };
 
         dispatcher.clone = function () {
-            // If you want to localize a generic to a new scope/module
-            // to define more methods on it.
-            //
-            //    var fn = module.fn.clone();
-            //    fn.defineMethod(...);
-            //
-            return new Generic(this.__default__, this.__table__, this.__overrides__);
+            return Generic.prototype.clone.apply(this, arguments);
         };
 
         return dispatcher;
+    };
+
+    Generic.prototype.dispatch = function (args, ctx) {
+        var i = this.__table__.length, method;
+
+        while (i--) {
+            method = this.__table__[i];
+
+            if (method.contract.argumentsMatch(args)) {
+                return method.apply(ctx, args); 
+            } 
+        }
+
+        return this.__default__.apply(this, args);
+    };
+
+    Generic.prototype.defineMethod = function (argTypes, fn) {
+        var contract, method, overrides;
+
+        contract = new Signature();
+        contract.argTypes = argTypes;
+        contract.returnType = Type.Any;
+
+        method = contract.wrap(fn);
+
+        this.__table__.push(method);
+        overrides = this.__overrides__;
+
+        // Maintain order of signature specificity
+        this.__table__.sort(function (a, b) {
+            var index_a, index_b, max, i;
+
+            for (i = 0, max = overrides.length; i < max; i++) {
+                index_a = overrides[i].indexOf(a);
+                index_b = overrides[i].indexOf(b);
+
+                if (index_a != -1 && index_b != -1) {
+                    return index_a < index_b ? MORE : LESS;
+                }
+            }
+
+            return WrappedFunction.moreSpecificThan(a, b) ? MORE : LESS;
+        });
+    };
+
+    Generic.prototype.defineMethods = function () {
+        var max = arguments.length - 1,
+            i;
+
+        for (i = 0; i < max; i += 2) {
+            this.defineMethod.call(this, arguments[i], arguments[i + 1]);
+        }
+    };
+
+    Generic.prototype.prefer = function (a, b) {
+        this.__overrides__.unshift([a, b]); 
+    };
+
+    Generic.prototype.clone = function () {
+        // If you want to localize a generic to a new scope/module
+        // to define more methods on it.
+        //
+        //    var fn = module.fn.clone();
+        //    fn.defineMethod(...);
+        //
+        return new Generic(this.__default__, this.__table__, this.__overrides__);
     };
 
     NoMatchingMethodError = function (args) {
